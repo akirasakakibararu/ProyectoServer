b28acfb649a4a3567e24af3f2726bf390c4a25eb
Daniel Caballero <danieledgar.caballerohu@elorrieta-errekamari.com>
2025-03-27 10:04:45 +0100

    Arreglo de objeto usuario

--- server/src/main/java/pojos/Usuarios.java
+++ server/src/main/java/pojos/Usuarios.java
@@ -1,11 +1,6 @@
 package pojos;
 
-import java.util.HashSet;
-import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -13,110 +8,121 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuarios")
 public class Usuarios implements java.io.Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_usuario")
-    private int idUsuario;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
+	private int idUsuario;
 
-    @Column(nullable = false, length = 255)
-    private String contrasena;
+	@Column(nullable = false, length = 255)
+	private String contrasena;
 
-    @Column(nullable = false, length = 100)
-    private String nombre;
+	@Column(nullable = false, length = 100)
+	private String nombre;
 
-    @Column(nullable = false, length = 100, unique = true)
-    private String email;
+	@Column(nullable = false, length = 100, unique = true)
+	private String email;
 
-    @Column(name = "foto_perfil")
-    @JsonIgnore
-    private byte[] fotoPerfil;
+	@Column(name = "foto_perfil")
+	private String fotoPerfil;
 
-    @Column(name = "habilitado")
-    private boolean habilitado = true;
+	@Column(name = "habilitado")
+	private boolean habilitado = true;
 
-    public enum Rol {
-        Empleado,
-        Administrador
-    }
+	@Enumerated(EnumType.STRING)
+	@Column(name = "rol", nullable = false)
+	private Rol rol;
 
-   public Usuarios() {
-	   
-   }
-    public Usuarios(String nombre, String contrasena) {
-        this.nombre = nombre;
-        this.contrasena = contrasena;
-    }
-    public Usuarios(String contrasena, String nombre, String email, Rol rol) {
-        this.contrasena = contrasena;
-        this.nombre = nombre;
-        this.email = email;
-       
-    }
+	public enum Rol {
+		Empleado, Administrador
+	}
 
-    public Usuarios(String contrasena, String nombre, String email, byte[] fotoPerfil, Rol rol, boolean habilitado) {
-        this.contrasena = contrasena;
-        this.nombre = nombre;
-        this.email = email;
-        this.fotoPerfil = fotoPerfil;
-        
-        this.habilitado = habilitado;
-    }
+	public Usuarios() {
 
+	}
 
-    public int getIdUsuario() {
-        return idUsuario;
-    }
+	public Usuarios(String nombre, String contrasena) {
+		this.nombre = nombre;
+		this.contrasena = contrasena;
+	}
 
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
-    }
+	public Usuarios(String contrasena, String nombre, String email, Rol rol) {
+		this.contrasena = contrasena;
+		this.nombre = nombre;
+		this.email = email;
+		this.rol = rol;
 
-    public String getContrasena() {
-        return contrasena;
-    }
+	}
 
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
+	public Usuarios(String contrasena, String nombre, String email, String fotoPerfil, Rol rol, boolean habilitado) {
+		this.contrasena = contrasena;
+		this.nombre = nombre;
+		this.email = email;
+		this.fotoPerfil = fotoPerfil;
+		this.habilitado = habilitado;
+		this.rol = rol;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public int getIdUsuario() {
+		return idUsuario;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getContrasena() {
+		return contrasena;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
 
-    public byte[] getFotoPerfil() {
-        return fotoPerfil;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setFotoPerfil(byte[] fotoPerfil) {
-        this.fotoPerfil = fotoPerfil;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public boolean isHabilitado() {
-        return habilitado;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setHabilitado(boolean habilitado) {
-        this.habilitado = habilitado;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFotoPerfil() {
+		return fotoPerfil;
+	}
+
+	public void setFotoPerfil(String fotoPerfil) {
+		this.fotoPerfil = fotoPerfil;
+	}
+
+	public boolean isHabilitado() {
+		return habilitado;
+	}
+
+	public void setHabilitado(boolean habilitado) {
+		this.habilitado = habilitado;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
 
 }
